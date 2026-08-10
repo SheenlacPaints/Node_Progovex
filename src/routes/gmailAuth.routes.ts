754a664833a9a235c3dbf340aab8e70240e05cb1
VinothKumar-Loganathan <vinothkumar.loganathan@itworks1ders.in>
2026-08-10 15:46:43 +0530

    init

--- src/routes/gmailAuth.routes.ts
+++ src/routes/gmailAuth.routes.ts
@@ -10,7 +10,7 @@ const SESSION_SECRET = process.env.SESSION_SECRET || 'dev-secret-change-in-produ
 const getFrontendUrl = () => (process.env.FRONTEND_URL || 'http://localhost:4200').replace(/\/+$/, '');
 
 const redirectToFrontend = (res: Response, query: string) => {
-  const url = `${getFrontendUrl()}/#/user/email?${query}`;
+  const url = `https://devtaskflow.sheenlac.com/#/user/email?${query}`;
   console.log('[GmailAuth] Redirecting to frontend:', url);
   return res.redirect(url);
 };
